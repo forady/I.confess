70587eb10ea57a7dd70956c421dbf901bcbbf089
forady <158487774+forady@users.noreply.github.com>
2024-02-21 12:17:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// script.js
+
 document.getElementById('yesButton').addEventListener('click', function() {
     var newDesign = document.getElementById('newDesign');
     newDesign.style.display = 'block';
 });
+
 document.getElementById('open-overlay1').addEventListener('click', function() {
     document.getElementById('overlay1').style.display = 'flex';
 });
@@ -15,29 +18,4 @@ var photos = document.getElementsByClassName('photo');
 
 function moveButton() {
     photos[currentPhotoIndex].style.display = 'none';
-    currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
-    photos[currentPhotoIndex].style.display = 'block';
-
-    var button = document.getElementById("noButton");
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
-
-    var maxX = windowWidth - button.offsetWidth;
-    var maxY = windowHeight - button.offsetHeight;
-
-    var randomX = Math.floor(Math.random() * maxX);
-    var randomY = Math.floor(Math.random() * maxY);
-
-    button.style.left = randomX + 'px';
-    button.style.top = randomY + 'px';
-}
-
-function closeOverlay() {
-    var overlay = document.getElementById('overlay');
-    overlay.classList.remove('active');
-}
-
-window.onload = function() {
-    var overlay = document.getElementById('overlay');
-    overlay.classList.add('active');
-};
+    currentPhotoIndex = (
